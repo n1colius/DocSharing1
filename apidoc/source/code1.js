85d5b0fc2ab09282ee54c5a44a76dee370340190
Nikolius <nikolius.lau@koltiva.com>
2019-01-15 19:57:31 +0700

    Tambah apidoc

---
+++ apidoc/source/code1.js
@@ -0,0 +1,28 @@
+/**
+ *  @api {post} https://niko.com/api/user Create a new User
+ *  @apiName PostUser
+ *  @apiGroup User
+ *
+ *  @apiDescription In this case "apiUse" is defined and used.
+ *  Define blocks with params that will be used in several functions, so you dont have to rewrite them.
+ *
+ *  @apiParam {String} name Name of the User.
+ *
+ *  @apiSuccess {String} id The new Users-ID.
+ */
+
+
+ /**
+ *  @api {get} https://niko.com/api/read_user Zet list of User
+ *  @apiName GetReadUser
+ *  @apiGroup User Admin
+ *
+ *  @apiDescription In this case "apiUse" is defined and used.
+ *  Define blocks with params that will be used in several functions, so you dont have to rewrite them.
+ *
+ *  @apiParam {String} name Name of the User.
+ *
+ *  @apiSuccess {String} id The new Users-ID.
+ * 
+ *  @apiError TakAda IDCoba Penjelasankah ini
+ */
---
+++ apidoc/source/code2.js
@@ -0,0 +1,28 @@
+/**
+ *  @api {get} https://niko.com/api/read_user Get list of User
+ *  @apiName GetReadUser2
+ *  @apiGroup User Admin
+ *
+ *  @apiDescription In this case "apiUse" is defined and used.
+ *  Define blocks with params that will be used in several functions, so you dont have to rewrite them.
+ *
+ *  @apiParam {String} name Name of the User.
+ *
+ *  @apiSuccess {String} id The new Users-ID.
+ * 
+ *  @apiError TakAda IDCoba Penjelasankah ini
+ */
+
+ 
+/**
+ *  @api {post} https://niko.com/api/user Create a new User
+ *  @apiName PostUser2
+ *  @apiGroup User
+ *
+ *  @apiDescription In this case "apiUse" is defined and used.
+ *  Define blocks with params that will be used in several functions, so you dont have to rewrite them.
+ *
+ *  @apiParam {String} name Name of the User.
+ *
+ *  @apiSuccess {String} id The new Users-ID.
+ */
